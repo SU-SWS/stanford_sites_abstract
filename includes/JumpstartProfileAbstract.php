d944dd843df691818d536800c7d414e7cd4fd8f9
Shea McKinney <sherakama@gmail.com>
2014-05-01 15:59:58 -0700

    Added function to include importer library files

--- includes/JumpstartProfileAbstract.php
+++ includes/JumpstartProfileAbstract.php
@@ -256,6 +256,25 @@ class JumpstartProfileAbstract extends JumpstartProfile {
     menu_position_add_rule($rule);
   }
 
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Loads the content importer library.
+   */
+  protected function load_sites_content_importer_files(&$install_state) {
+    // Try to use libraries module if available to find the path.
+    if (function_exists('libraries_get_path')) {
+      $library_path = DRUPAL_ROOT . '/' . libraries_get_path('stanford_sites_content_importer');
+    }
+    if (!file_exists($library_path . '/SitesContentImporter.php')) {
+      $library_path = DRUPAL_ROOT . '/sites/all/libraries/stanford_sites_content_importer';
+    }
+    if (!file_exists($library_path . '/SitesContentImporter.php')) {
+      $library_path = DRUPAL_ROOT . '/sites/default/libraries/stanford_sites_content_importer';
+    }
+    $library_path .= "/SitesContentImporter.php";
+    include_once $library_path;
+  }
 
 
 }
